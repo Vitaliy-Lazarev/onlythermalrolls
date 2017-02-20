bd2f78b7ffdfff08587ee36f4adbaa7f68a6fcf3
vlazarev <vlazarev@magecom.net>
2017-02-20 19:05:27 +0200

    file project

---
+++ gulpfile.js/shopify-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+
+    shopifyCredentials: {
+        api_key: '2117cc13729c4a36bec3c770b10c2a11',
+        password: '3b2e7a74f582eb69c6f01e7762484895',
+        url: 'onlythermalrolls.myshopify.com',
+        theme_id: '160541394'
+    }
+};
+
