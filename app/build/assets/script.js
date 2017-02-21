d7205a71950620a3f2f9058529f29d9da0c428e0
vlazarev <vlazarev@magecom.net>
2017-02-21 18:22:37 +0200

    setting theme

--- app/build/assets/script.js
+++ app/build/assets/script.js
@@ -1,41 +1,44 @@
 jQuery(document).ready(function($) {
-  
-  
-$('.btnAddCart').on('click', function(event) {
-    event.preventDefault();
-  $( "#AddToCart-product-template" ).trigger( "click" );
-});
 
-// tabs pagew products
-$('ul.tabs').each(function(){
-      var active, content, links = $(this).find('a');
-      active = links.first().addClass('active');
-      content = $(active.attr('href'));
-      links.not(':first').each(function () {
-        $($(this).attr('href')).hide();
-      });
-      $(this).find('a').click(function(e){
-        active.removeClass('active');
-        content.hide();
-        active = $(this);
-        content = $($(this).attr('href'));
-        active.addClass('active');
-        content.show();
-        return false;
-      });
-});
+    (function ($) {
+        // tabs page products
+        $('ul.tabs').each(function(){
+            var active, content, links = $(this).find('a');
+            active = links.first().addClass('active');
+            content = $(active.attr('href'));
+            links.not(':first').each(function () {
+                $($(this).attr('href')).hide();
+            });
+            $(this).find('a').click(function(e){
+                active.removeClass('active');
+                content.hide();
+                active = $(this);
+                content = $($(this).attr('href'));
+                active.addClass('active');
+                content.show();
+                return false;
+            });
+        });
+    })($);
 
-$('#add-review').on('click', function (event) { // если в href начинается с # или ., то ловим клик
-    event.preventDefault();
-$( "#box-reviews a" ).trigger( "click" );
-var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href
-if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
-$('html, body').animate({
-scrollTop: $(scroll_el).offset().top
-}, 1100); // анимируем скроолинг к элементу scroll_el
-}
-return false; // выключаем стандартное действие
-});
+
+// $('.btnAddCart').on('click', function(event) {
+//     event.preventDefault();
+//   $( "#AddToCart-product-template" ).trigger( "click" );
+// });
+
+
+// $('#add-review').on('click', function (event) { // если в href начинается с # или ., то ловим клик
+//     event.preventDefault();
+// $( "#box-reviews a" ).trigger( "click" );
+// var scroll_el = $(this).attr('href'); // возьмем содержимое атрибута href
+// if ($(scroll_el).length != 0) { // проверим существование элемента чтобы избежать ошибки
+// $('html, body').animate({
+// scrollTop: $(scroll_el).offset().top
+// }, 1100); // анимируем скроолинг к элементу scroll_el
+// }
+// return false; // выключаем стандартное действие
+// });
 
 
  // $('.site-nav--has-dropdown').hover(function () {
@@ -44,13 +47,13 @@ return false; // выключаем стандартное действие
  //     $(this).removeClass('active-menu').find('.site-nav__dropdown').stop(true, true).delay(300).fadeOut(200);
  // });
 
-    if($('body').hasClass('template-collection')){
-        $('#collection-description').readmore({
-            collapsedHeight: 110,
-            speed: 200,
-            lessLink: '<a href="#">Read less</a>',
-        });
-    }
+    // if($('body').hasClass('template-collection')){
+    //     $('#collection-description').readmore({
+    //         collapsedHeight: 110,
+    //         speed: 200,
+    //         lessLink: '<a href="#">Read less</a>',
+    //     });
+    // }
 });
 
 
